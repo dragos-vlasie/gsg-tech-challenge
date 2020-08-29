83d3c41071b6aa43629ce4eb1af271944cf4df20
Dragos Vlasie <37800900+dragos-vlasie@users.noreply.github.com>
2020-08-29 17:42:59 +0700

    pass menuData to menuOption component and display it using recursion

--- src/components/MenuOptions.js
+++ src/components/MenuOptions.js
@@ -1,6 +1,14 @@
 import React from "react";
 
 export const MenuOptions = ({ data }) => {
-  return <div className="MenuOptions">{data.label}</div>;
+  const nestedItems =
+    data.children &&
+    data.children.map((nestedItem, index) => <MenuOptions key={"ingredient-" + index} data={nestedItem} />);
+  return (
+    <div className="MenuOptions">
+      <h3>{data.label}</h3>
+      {nestedItems}
+    </div>
+  );
 };
 export default MenuOptions;
